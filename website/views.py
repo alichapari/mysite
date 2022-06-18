b7609a88f8be9b921184f364e132b146590ba0fb
alichapari <107399562+alichapari@users.noreply.github.com>
2022-06-18 13:05:44 +0430

    templates
    i creat a template very fun

--- website/views.py
+++ website/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 from django.http import HttpResponse , JsonResponse
 
 def index_view (request):
-    return HttpResponse('<h1>homw page </h1>')
+    return render(request , 'website/index.html')
 
 
 def about_view(request):
-    return HttpResponse('hi my name is jeff')
+    return render(request , 'website/about.html')
 
 def contact_view(request):
-    return HttpResponse('this is my telegram')
+    return render(request , 'website/contact.html')
 
