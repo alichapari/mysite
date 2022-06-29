65918526854b9887a9a3d99973f99ff8ff655c60
alichapari <107399562+alichapari@users.noreply.github.com>
2022-06-29 20:59:44 +0430

    we did some thing

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from venv import create
 from django.db import models
 
 # Create your models here.
@@ -5,4 +6,14 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    
+    counted_views = models.IntegerField(default=0)
+
+    status = models.BooleanField(default=False)
+    published_date= models.DateTimeField(null=True)
+    created_date= models.DateTimeField(auto_now_add=True)
+    updated_date= models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self) :
+        return self.title
--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from website.models import Contact
 # Register your models here.
+class ContanctAdmin(admin.ModelAdmin):
+    date_hierarchy ='created_date'
+    #empty_value_display = '-empty-'
+   
+    list_display = ('name','email','created_date',)
+
+    list_filter = ('email',)
+    #ordering = ['-created_date']
+    search_fields = ('name' , 'message',)
+
+
+admin.site.register(Contact,ContanctAdmin)
